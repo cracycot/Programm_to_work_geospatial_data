776e5e3b627bdb490e0c03736ec251a0811470fd
kirilllesniak <cracycot@mail.ru>
2023-02-07 15:19:49 +0300

    commit 07.02.23 4

--- first.py
+++ first.py
@@ -9,6 +9,7 @@ import h5py
 
 
 
+
 import numpy as np
 import math
 import matplotlib.pyplot as plt
--- klim_cod.py
+++ klim_cod.py
@@ -15,6 +15,7 @@ hdf_14 = SD(filename_14, SDC.READ)
 #otkrivayut fayli, format pyhdf
 
 
+
 FP_row = hdf_14.select('FP_line')
 FP_col = hdf_14.select('FP_sample')
 #poluchayut stroki po imeni
